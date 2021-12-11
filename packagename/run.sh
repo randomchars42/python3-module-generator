b17ae558b85f9b38dde9a2fc113c1ad3de4691b0
randomchars42 <randomchars42@users.noreply.github.com>
2021-12-11 11:15:51 +0100

    fix location

--- packagename/run.sh
+++ packagename/run.sh
@@ -1,12 +1,4 @@
-#!/usr/bin/env bash
+#/usr/bin/env bash
 
-# approach taken from:
-# https://codefather.tech/blog/bash-get-script-directory/
 script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-current_dir=$PWD
-# go back to the current directory even if the script is interrupted
-trap "cd $PWD" EXIT
-
-cd "$script_dir/src"
-python3 -m $PACKAGENAME.$PACKAGENAME "$@"
-cd $PWD
+PYTHONPATH=$PYTHONPATH:"$script_dir"/src python3 -m $PACKAGENAME.$PACKAGENAME "$@"
--- run.sh
+++
@@ -1,4 +0,0 @@
-#/usr/bin/env bash
-
-script_dir=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-PYTHONPATH=$PYTHONPATH:"$script_dir"/src python3 -m $PACKAGENAME.$PACKAGENAME
